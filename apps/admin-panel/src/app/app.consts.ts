bc455846467f0a7738eba8df6a4d1b31841a4966
Anastasia Andreeva <frontaana@gmail.com>
2026-04-22 01:58:03 +0300

    added tasks list component, routing fix

---
+++ apps/admin-panel/src/app/app.consts.ts
@@ -0,0 +1,3 @@
+export enum RouteName {
+  taskManager = 'task-manager',
+}
---
+++ apps/admin-panel/src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,76 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DistributeColorPipe } from '../../pipes/distribute-color.pipe';
+
+enum Categories {
+  home = 'Home',
+  beauty = 'Beauty',
+  work = 'Work',
+  food = 'Food',
+}
+
+enum Priority {
+  low = 'Low',
+  medium = 'Medium',
+  high = 'High',
+}
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [DatePipe, DistributeColorPipe],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.scss',
+})
+export class TaskListComponent implements OnInit {
+  public randomOffset = 0;
+  public tasksMap = new Map();
+
+  protected categoriesArray = Object.values(Categories).filter(
+    (v) => typeof v === 'string'
+  );
+
+  private tasks = [
+    {
+      category: Categories.home,
+      title: 'Помыть полы',
+      priority: Priority.medium,
+    },
+    {
+      category: Categories.work,
+      title: 'Резюме',
+      priority: Priority.high,
+    },
+    {
+      category: Categories.beauty,
+      title: 'Сходить в спортзал',
+      priority: Priority.high,
+    },
+    {
+      category: Categories.food,
+      title: 'Составить меню',
+      priority: Priority.medium,
+    },
+  ];
+
+  ngOnInit() {
+    this.randomOffset = Math.floor(Math.random() * 360);
+
+    this.categoriesArray.forEach((category) => {
+      this.tasksMap.set(
+        category,
+        this.tasks.filter((task) => task.category === category)
+      );
+    });
+  }
+
+  getRandomColor() {
+    return `hsl(${Math.floor(Math.random() * 360)}, 70%, 50%)`;
+  }
+
+  getNextDay(item: number): Date {
+    const date = new Date();
+    date.setDate(date.getDate() + +item);
+    return date;
+  }
+}
