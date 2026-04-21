7352c683858ab9628789e2275455b3caa2098475
Anastasia Andreeva <frontaana@gmail.com>
2026-04-22 02:30:20 +0300

    added tasks material styles to header c

--- apps/admin-panel/src/app/components/task-manager/task-manager.component.ts
+++ apps/admin-panel/src/app/components/task-manager/task-manager.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DistributeColorPipe } from '../../pipes/distribute-color.pipe';
 
 @Component({
   selector: 'app-task-manager',
   standalone: true,
-  imports: [DistributeColorPipe, RouterOutlet],
+  imports: [
+    DistributeColorPipe,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './task-manager.component.html',
   styleUrl: './task-manager.component.scss',
 })
-export class TaskManagerComponent {
-  public headerIndex = 999;
-  public randomOffset = 0;
-}
+export class TaskManagerComponent {}
